da94b0ff308f7039e886da7dca985f99df624671
apinan90928 <apinan90928@gmail.com>
2020-06-23 09:00:32 +0700

    IMPORTING PROJECT

---
+++ VS_GITHUB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VS_GITHUB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
